479cb93e1c60a4c6b8d375bbdc7b87b9b49d7834
fercho9212 <ferney9212@gmail.com>
2016-12-13 22:23:40 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Repositorio de repos</title>
+        <title>new laravel this yes</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                  This is a test for git hub :D
+                 helloo, this is mi first app :D
                 </div>
 
                 <div class="links">
